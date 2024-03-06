8f7bd09a155c7ea200c15596edf796aad425e24b
ManuelMorenoCámara <manuel.moreno@babelgroup.com>
2024-03-06 21:33:42 +0100

    Design

--- PadelKuatro/PadelKuatro/Scenes/Register/RegisterView.swift
+++ PadelKuatro/PadelKuatro/Scenes/Register/RegisterView.swift
@@ -11,8 +11,77 @@ struct RegisterView: View {
 
     @ObservedObject var viewModel: RegisterViewModel
 
+    @FocusState private var emailIsFocused: Bool
+    @FocusState private var passwordIsFocused: Bool
+    @FocusState private var confirmPasswordIsFocused: Bool
+
+    let screenHeight = UIScreen.main.bounds.height
+    let screenwidth = UIScreen.main.bounds.width
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .fill(.black)
+                .ignoresSafeArea()
+                .opacity(0.8)
+            VStack {
+                HStack {
+                    Button {
+                        viewModel.navigateToLogin()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                    }.padding(.leading, screenwidth / -5)
+
+                    Text("register_title".localized)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.red)
+                }
+                Form {
+                    Section{
+                        TextField("register_email".localized, text: $viewModel.email)
+                    }
+                    .focused($emailIsFocused)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                    .keyboardType(.emailAddress)
+                    
+                    Section{
+                        TextField("register_password".localized, text: $viewModel.password)
+                    }
+                    .focused($passwordIsFocused)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+
+                    Section{
+                        TextField("register_confirm_password".localized, text: $viewModel.confirmPassword)
+                    }
+                    .focused($confirmPasswordIsFocused)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                }
+                .scrollDisabled(true)
+                .padding(.vertical, 10)
+                .scrollContentBackground(.hidden)
+                .background(Color.clear.ignoresSafeArea())
+                Button {
+
+                }
+                label: {
+                    Text("register_register_button_title".localized)
+                }
+                .buttonStyle(RoundedButton(color: Constants.Colors.purpleButton))
+                .padding(.bottom, 20)
+            }.onTapGesture {
+                setFocusedFieldsOff()
+            }
+        }
+    }
+
+    func setFocusedFieldsOff() {
+        emailIsFocused = false
+        passwordIsFocused = false
+        confirmPasswordIsFocused = false
     }
 }
 
--- PadelKuatro/PadelKuatro/Scenes/Register/RegisterViewModel.swift
+++ PadelKuatro/PadelKuatro/Scenes/Register/RegisterViewModel.swift
@@ -8,5 +8,12 @@
 import SwiftUI
 
 class RegisterViewModel: BaseViewModel<MainCoordinatorProtocol>, ObservableObject {
-    
+
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var confirmPassword: String = ""
+
+    func navigateToLogin() {
+        getCoordinator()?.navigateToLogin()
+    }
 }

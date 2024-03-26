e6c17a996a35a73595233e19ecde119c9a4f47ab
ManuelMorenoCámara <manuel.moreno@babelgroup.com>
2024-03-26 17:03:02 +0100

    Wip

--- PadelKuatro/PadelKuatro/Scenes/RememberPasswordView/RememberPasswordView.swift
+++ PadelKuatro/PadelKuatro/Scenes/RememberPasswordView/RememberPasswordView.swift
@@ -11,6 +11,8 @@ struct RememberPasswordView: View {
 
     @ObservedObject var viewModel: RememberPasswordViewModel
 
+    @FocusState private var emailIsFocused: Bool
+
     private let screenWidth = UIScreen.main.bounds.width
 
     var body: some View {
@@ -27,24 +29,42 @@ struct RememberPasswordView: View {
                         Image(systemName: "chevron.left")
                     }
                     .padding(.leading, -30)
-                    Text("Recuperar Contraseña")
+                    Text("rememberPassword_Title")
                         .font(.title)
                         .bold()
                         .foregroundColor(.red)
                 }
                 Spacer()
-                Text("rememberPassword_description".localized)
+                Text("rememberPassword_description")
                     .font(.title3)
                     .padding()
                     .foregroundColor(.white)
+                Form {
+                    Section{
+                        TextField("register_email", text: $viewModel.email)
+                    }
+                    .focused($emailIsFocused)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                    .keyboardType(.emailAddress)
+                }
+                .scrollDisabled(true)
+                .scrollContentBackground(.hidden)
+                .background(Color.clear.ignoresSafeArea())
                 Spacer()
                 Button {
                     viewModel.rememberPassword()
                 } label: {
-                    Text("Restablecer contraseña")
+                    Text("rememberPassword_resetPassword")
                 }
                 .buttonStyle(RoundedButton(color: .purple))
                 .padding(.bottom, 30)
+                .alert(isPresented: $viewModel.showAlert) {
+                    ShowAlert().show(alert: viewModel.alertType ?? .notMatchPassword, completion: viewModel.getCoordinator()!.navigateToLogin)
+                }
+            }
+            .onTapGesture {
+                emailIsFocused = false
             }
         }
     }
--- PadelKuatro/PadelKuatro/Scenes/RememberPasswordView/RememberPasswordViewModel.swift
+++ PadelKuatro/PadelKuatro/Scenes/RememberPasswordView/RememberPasswordViewModel.swift
@@ -6,11 +6,44 @@
 //
 
 import SwiftUI
+import Firebase
 
 class RememberPasswordViewModel: BaseViewModel<MainCoordinatorProtocol>, ObservableObject {
 
+    @Published var email: String = ""
+    @Published var showAlert: Bool = false
+    @Published var alertType: LogInAlertType?
+
     func rememberPassword() {
-        // TODO: - Remember Password service call
+        if email.isEmail {
+            /*Auth.auth().fetchSignInMethods(forEmail: email) { [weak self] (signInMethods, error) in
+                guard let self else { return }
+                if let _ = error {
+                    self.alertType = .rememberPasswordError
+                    self.showAlert = true
+                    return
+                }
+                if let signInMethods = signInMethods {
+                    if signInMethods.isEmpty {
+                        self.alertType = .notAnUser
+                        self.showAlert = true
+                    } else {*/
+                        Auth.auth().sendPasswordReset(withEmail: self.email) { error in
+                            if let _ = error {
+                                self.alertType = .rememberPasswordError
+                                self.showAlert = true
+                            } else {
+                                self.alertType = .successRememberPassword
+                                self.showAlert = true
+                            }
+                        }
+                    //}
+                //}
+            //}
+        } else {
+            alertType = .wrongEmail
+            showAlert = true
+        }
     }
 
     func navigatetToLoging() {
